93f5a6e2daeeedb3609dbc8279e4d79af13d631a
ailevchenko <levchenko.ai305@gmail.com>
2021-03-04 20:35:26 +0300

    lesson-4

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,11 @@
+module.exports.homelist = (req, res) => {
+    res.render('locations-list', {title: 'Home'});
+}
+
+module.exports.locationInfo = (req, res) => {
+    res.render('locations-list', {title: 'Location info'});
+}
+
+module.exports.addReview = (req, res) => {
+    res.render('locations-list', {title: 'Add review'});
+}
